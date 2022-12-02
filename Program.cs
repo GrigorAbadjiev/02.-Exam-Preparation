8f582598d4946b3ac23e281f84378b5b2fad3e02
User <User@DESKTOP-ESP2JT1>
2022-12-02 13:32:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _02._Exam_Preparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxBadGradesCount = int.Parse(Console.ReadLine());
+            int currentBadGradesCount = 0;
+            string LastProblemName = "";
+            int solvedProblems = 0;
+            int gradesSum = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Enough")
+                {
+                    break;
+                }
+
+                LastProblemName = command;
+                solvedProblems++;
+                int grade = int.Parse(Console.ReadLine());
+                gradesSum += grade;
+               
+                if (grade <= 4)
+                {
+                    currentBadGradesCount++;
+                }
+
+                if (maxBadGradesCount == currentBadGradesCount)
+                {
+                    break;
+                }
+            }
+
+            if (maxBadGradesCount == currentBadGradesCount)
+            {
+                Console.WriteLine($"You need a break, {currentBadGradesCount} poor grades.");
+            }
+            else
+            {
+                double averageGrade = gradesSum * 1.0 / solvedProblems;
+                Console.WriteLine($"Average score: {averageGrade:f2}");
+                Console.WriteLine($"Number of problems: {solvedProblems}");
+                Console.WriteLine($"Last problem: {LastProblemName}");
+            }
+        }
+    } 
+}
